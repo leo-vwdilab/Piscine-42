cb84f8f484720cb44e67f3251ad782bdfbaebb80
Leo Dias <40175531+leo-vwdilab@users.noreply.github.com>
2021-01-30 23:31:49 +0000

    Create ft_boolean.h

---
+++ C07/ex01/ft_boolean.h
@@ -0,0 +1,15 @@
+#ifndef MERDA
+#define MERDA
+
+#include <unistd.h>
+
+typedef int t_bool;
+
+#define EVEN(n) n % 2 == 0
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "fixe"
+#define ODD_MSG "cenas"
+#define SUCCESS 0
+
+#endif
